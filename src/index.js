3b8ce368f56663c56fd064505a57b34ef76f8013
luke <543564013@qq.com>
2017-12-26 21:44:31 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+const Root = () => (
+	<Router history={browserHistory}>
+		<Route path="/" component={App} />
+	</Router>  
+)
+import reducers from './reducers'
+let store = createStore(reducers)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('root'))
---
+++ src/reducers/todo.js
@@ -0,0 +1,17 @@
+function todoApp(state = {}, action) {
+	switch (action.type) {
+	case 'ADD_TODO':
+		return Object.assign({}, state, {
+			todos: [
+				...state.todos,
+				{
+					text: action.text,
+					completed: false
+				}
+			]
+		})
+	default:
+		return state
+	}
+}
+module.exports.todo = todoApp
