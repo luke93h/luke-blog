c3db100387668ef0decf49b2a87b043baf7213fa
luke <543564013@qq.com>
2017-12-30 22:14:39 +0800

    完成全屏化动画

--- src/components/mainBox/index.js
+++ src/components/mainBox/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styles from './index.css'
 import Hammer from 'react-hammerjs';
+import requestAnimFrame from '../../utils/requestAnimFrame.js'
 class Box extends Component {
     constructor(props){
         super(props)
@@ -8,7 +9,8 @@ class Box extends Component {
             activeClass: '',
             fullClass: '',
             position: {},
-            wrapperStyle: {}
+            wrapperStyle: {},
+            initPos:{}
         }
         this.isFull = false
     }
@@ -17,17 +19,11 @@ class Box extends Component {
             return
         }
         var rect = e.currentTarget.getBoundingClientRect()
+        this.rect = rect
         this.setState({
             activeClass: styles.active,
-            position: {
-                left: rect.left + 'px',
-                top: rect.top + 'px',
-                right: document.body.clientWidth - rect.right + 'px',
-                bottom: rect.bottom,
-                minHeight: rect.bottom - rect.top + 'px'
-            },
             wrapperStyle: {
-                height: rect.bottom -  rect.top + 'px'
+                height: rect.bottom - rect.top + 'px'
             }
         })
     }
@@ -40,29 +36,61 @@ class Box extends Component {
         })
     }
     onTap() {
+        if(this.isFull){
+            return
+        }
         this.isFull = true
+        var rect = this.rect
+        this.top = rect.top
         this.setState({
             fullClass: styles.full,
-            activeClass: ''
+            activeClass: '',
+            initPos: {
+                top: rect.top
+            }
         })
+        this.step((new Date()).valueOf())
+    }
+    step(timestamp) {
+        var start = this.start
+        if (!start) start = this.start = timestamp;
+        var progress = (new Date()).valueOf() - start;
+        this.setState({
+            fullClass: styles.full,
+            activeClass: '',
+            initPos: {
+                top: (1-progress/300)*this.top + 'px'
+            }
+        })
+        if (progress < 300) {
+            requestAnimFrame(this.step.bind(this));
+        }
     }
     render() {
+        // 三层包装，第一层做占位元素，防止后面元素顶上来
+        // 第二层，点开后作为容器，fixed充满屏幕
+        // 第三层为真实内容，做动画
         return (
             <div style={this.state.wrapperStyle} className={styles.wrapper}>
                 <Hammer 
                     // 通过css类添加最终覆盖页面的最终值
-                    className={`${styles.main} ${this.state.activeClass} ${this.state.fullClass}`}  
                     onTouchStart={this.onTouchStart.bind(this)} 
                     onTouchEnd={this.onTouchEnd.bind(this)}
                     onTap={this.onTap.bind(this)}
                     // 通过计算的得到的初始值
                     style={this.state.position}
                 >
-                    <div className={`${styles.body}`}>
-                        <div className={styles.head}>
-                            <img src="./images/img2.jpg" alt="" />
+                    <div className={`${styles.main} ${this.state.activeClass} ${this.state.fullClass}`}  >
+                        <div className={styles.body} style={this.state.initPos}>
+                            <div className={styles.head}>
+                                <img src="./images/img2.jpg" alt="" />
+                            </div>
+                            <div className={`${styles.content}`}>
+                                {
+                                    this.props.children
+                                }
+                            </div>
                         </div>
-                        <div className={styles.content}></div>
                     </div>
                 </Hammer>
             </div>
---
+++ src/utils/requestAnimFrame.js
@@ -0,0 +1,9 @@
+var requestAnimFrame = (function(){
+    return  window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+export default requestAnimFrame
