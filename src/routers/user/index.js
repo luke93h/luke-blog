a8e53e1537ac2e94aa9cc699ab1038b684a33b66
luke <543564013@qq.com>
2018-01-11 19:48:39 +0800

    更新了个人中心

--- src/routers/user/index.js
+++ src/routers/user/index.js
@@ -10,6 +10,27 @@ class Articles extends Component {
             >
                 <div className={styles.head}>
                     <div className={styles.title}>LUKE</div>
+                    <div className={styles.user}>
+                        <img src={require('../../assets/user.jpg')} alt=""/>
+                    </div>
+                    <div className={styles.headFoot}>
+                        <span className='iconfont icon-liulan' style={{marginRight: '0.1rem'}}>
+                        </span>
+                        浏览量： 88次
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.tabs}>
+                        <div className={styles.tab}>
+                            关于我
+                        </div>
+                        <div className={styles.tab}>
+                            联系方式
+                        </div>
+                        <div className={styles.tab}>
+                            占位3
+                        </div>
+                    </div>
                 </div>
             </div>
             
